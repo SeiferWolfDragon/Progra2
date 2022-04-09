73e970e93cc81c35bd0774735d4a1ffbc785d565
GiovanniJairSanchezChimal <ghostgs.117@gmail.com>
2022-04-08 19:27:02 -0500

    actualizacion

--- csv2sql.py
+++ csv2sql.py
@@ -3,9 +3,8 @@ import json
 #import SQL
 from pathlib import Path
 Conf=None
-with open("configuracion.json") as jsonfile:
+with open("configuracion_prueba.json") as jsonfile:
     Conf=json.load(jsonfile)
-
 ArchivoEntrada=Conf['ArchivoEntrada']#lleva corchete porqu estamos en diccionario
 ArchivoSalida=Conf['ArchivoSalida']
 Tabla=Conf['Tabla']
@@ -14,6 +13,8 @@ Columnas=Conf["Columnas"]
 #def numColumnas(fila,columnas):
     
 
+
+
 print(f"abriendo archivo: {ArchivoEntrada}...")
 RutaArchivoEntrada = Path(ArchivoEntrada)
 if RutaArchivoEntrada.exists():
@@ -21,6 +22,7 @@ if RutaArchivoEntrada.exists():
         print("Se ha Encontrado el archivo")
         lectorCSV=csv.reader(archivocsv,delimiter=',')
         for fila in lectorCSV:
-            print(fila)
+            Columnas.append(fila)
+            print(Columnas)
 else:
     print("no pude abrir el archivo {}".format(ArchivoEntrada))
