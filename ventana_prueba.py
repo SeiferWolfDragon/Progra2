3563363813ea4acb1c18c4ccacd5bd8fdfd2ab45
GiovanniJairSanchezChimal <ghostgs.117@gmail.com>
2022-05-05 18:06:56 -0500

    Se modifica el codigo para la conneccion con la base de datos

--- ventana_prueba.py
+++ ventana_prueba.py
@@ -2,95 +2,86 @@
 """
 Created on Wed May  4 15:23:27 2022
 
-@author: ghost
+@author: Giovanni Jair Sanchez Chimal
+        Juan Diego Vazquez Hernandez
 """
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from ColorLabel import ColorLabel
-import random
+from kivy.uix.textinput import TextInput
+import json
+import pymysql.cursors
 
 def byte2Per(nbyte):
 	return nbyte*(1/255)
 
 
-class sqlapp(App):
-    def btnaccion_press(self,obj):
-        gen = random.Random()
-        Dado1 = 0
-        Dado1 = gen.randint(1,6)
-        self.lblnumdado.text = str(Dado1)
-		
-	# Este es el constructor
-    def __init__(self,**kwargs):
-		# llamar al constructor de la clase base (App)
-        super().__init__(**kwargs)
-		#Defino un atributo
-		
+class Acceso(App):
+    def botonok_press(self,obj):
+        if self.connection:
+            nick = self.inputUsuario.text
+            password = self.inputUsuario.text
+            Mi_cursor = self.connection.cursor()
+            #SQL a ejecutar
+            SQL = 'Select * from usuario where nick = %s and password = sha2(%s,256)'
+            #Print Sql
+            Mi_cursor.execute(SQL,[nick,password])
+            Resultado = Mi_cursor.fetchone()
+            if Resultado:
+                self.columna2.text = 'Acceso Concedido'
+                print('Acceso Concedido')
+            else:self.columna2.text = 'Acceso no concedido'
+            print('Acceso no concedido')
+        else:
+            print('Error')
+    def iniciarBD(self):
+        self.conf= None
+        with open('db.json') as jsonfile:
+            self.conf = json.load(jsonfile)
+            
+        self.connection = pymysql.connect(
+            host=self.conf['HOST'], user=self.conf['DBUSR'],
+            password=self.conf['DBPASS'], database=self.conf['DBNAME'],
+            charset='utf8mb4', port=self.conf['PORT']
+            )
+        
     def build(self):
 		# Vamos a definir un layout
-        gdl_principal = GridLayout(cols=3)
+        self.gdl_principal = GridLayout(cols=3)
+        self.gdl_medio = GridLayout(rows =5)
 		############################################
-        lbltitulo = self.label = ColorLabel(text='hola')
-        self.label.background_color = [byte2Per(255),byte2Per(150),byte2Per(170),1]
+        self.labelusuario = ColorLabel(text='Usuario')
+        self.labelusuario.background_color=[1,0,0,1]
+        self.inputUsuario = TextInput(text='')
+        self.labelpassword = ColorLabel(text='Contraseña')
+        self.labelpassword.background_color=[1,0,0,1]
+        self.inputpassword = TextInput(text='')
+        self.botonok = Button(text='Accesar')
+        self.botonok.background_color=[0,0,0,1]
+        
 		############################################
-        gdl_principal.add_widget(lbltitulo)
+        self.gdl_medio.add_widget(self.labelusuario)
+        self.gdl_medio.add_widget(self.inputUsuario)
+        self.gdl_medio.add_widget(self.labelpassword)
+        self.gdl_medio.add_widget(self.inputpassword)
+        self.gdl_medio.add_widget(self.botonok)
+        #############################################
+        self.botonok.bind(on_press=self.botonok_press)
+        ###############################################33
+        columna1=ColorLabel(text='')
+        columna1.background_color=[0.5,0.7,1,1]
+        self.columna2=ColorLabel(text='')
+        self.columna2.background_color=[0.5,0.7,1,1]
+        self.gdl_principal.add_widget(columna1)
+        self.gdl_principal.add_widget(self.gdl_medio)
+        self.gdl_principal.add_widget(self.columna2)
+        return self.gdl_principal
         
-        #Grid medio
-        gdl_medio = GridLayout(rows=5)
-        lblresultado = self.label = ColorLabel(text='Resultado:')
-        self.label.background_color = [0.9,0,0.80,1]
-        gdl_medio.add_widget(lblresultado)
-        lblresultado1 = self.label = ColorLabel(text='Resultado1:')
-        self.label.background_color = [0.9,0,0.80,1]
-        gdl_medio.add_widget(lblresultado1)
-        lblresultado2 = self.label = ColorLabel(text='Resultado2:')
-        self.label.background_color = [0.9,0,0.80,1]
-        gdl_medio.add_widget(lblresultado2)
-        lblresultado3 = self.label = ColorLabel(text='Resultado3:')
-        self.label.background_color = [0.9,0,0.80,1]
-        gdl_medio.add_widget(lblresultado3)
-        lblresultado4 = self.label = ColorLabel(text='Resultado4:')
-        self.label.background_color = [0.9,0,0.80,1]
-        gdl_medio.add_widget(lblresultado4)
-
-
-        prueba = gdl_medio
-        self.label.background_color = [byte2Per(220),byte2Per(100),byte2Per(170),1]
-		############################################
-        gdl_principal.add_widget(prueba)
-        """
-		#Grid medio
-        gdl_medio = GridLayout(rows=5)
-        lblresultado = self.label = ColorLabel(text='Resultado:')
-        self.label.background_color = [0.9,0,0.80,1]
-        gdl_medio.add_widget(lblresultado)
-        lblnumdado = self.label = ColorLabel(text='hgfhg')
-        self.label.background_color = [1,0,1,1]
-        gdl_medio.add_widget(lblnumdado)
-        gdl_principal.add_widget(gdl_medio)
-        
-        lblresultado1 = self.label = ColorLabel(text='Resultado:')
-        self.label.background_color = [0.9,0,0.80,1]
-        gdl_medio.add_widget(lblresultado1)
-        lblnumdado1 = self.label = ColorLabel(text='hgfhg')
-        self.label.background_color = [1,0,1,1]
-        gdl_medio.add_widget(lblnumdado1)
-        lblnumdado2 = self.label = ColorLabel(text='hgfhg')
-        self.label.background_color = [1,0,1,1]
-        gdl_medio.add_widget(lblnumdado2)
-        gdl_principal.add_widget(gdl_medio)
-        """
-        
-		#Boton
-        btnaccion = Button(text="Presionanme!!!")
-        gdl_principal.add_widget(btnaccion)
-        btnaccion.bind(on_press = self.btnaccion_press)
-       # btnaccion.bind(on_press=self.btn_press)
-        self.gdl_principal = gdl_principal
-        #self.lblnumdado = lblnumdado
-        return gdl_principal
 
 if __name__ == '__main__':
-	D = sqlapp()
-	D.run()
+    SQLApp = Acceso()
+    SQLApp.iniciarBD()
+    SQLApp.run()
+    #Esto ejecuta el entrono grafico
+    
